3357f558f1403482abd4d72b2dec9b553d88a19d
Viktor <viktorhansen@me.com>
2016-08-10 11:07:30 +0200

    Bröt ut klasserna i server

---
+++ Networking_server/ClientHandler.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using static Networking_server.Program;
+
+namespace Networking_server
+{
+    class ClientHandler
+    {
+        public TcpClient tcpclient;
+        private Server myServer;
+        public string Name { get; set; }
+
+        public ClientHandler(TcpClient c, Server server)
+        {
+            tcpclient = c;
+            this.myServer = server;
+        }
+
+        public void Run()
+        {
+            try
+            {
+                string message = "";
+                bool firstLogin = true;
+
+                while (firstLogin)
+                {
+                    SetUserName();
+                    firstLogin = false;
+                }
+
+
+                while (!message.Equals("quit"))
+                {
+
+                    NetworkStream n = tcpclient.GetStream();
+                    message = new BinaryReader(n).ReadString();
+                    if (message.Split(' ')[0].ToLower().Equals("pm"))
+                    {
+                        var msg = "";
+                        for (int i = 0; i < message.Split(' ').Length; i++)
+                        {
+                            if (i > 1)
+                            {
+                                msg += message.Split(' ')[i] + " ";
+                            }
+                        }
+                        myServer.BroadcastPrivate(this, msg, message.Split(' ')[1]);
+                    }
+                    else
+                    {
+                        myServer.Broadcast(this, message);
+                    }
+                    Console.WriteLine(message);
+                }
+
+                myServer.DisconnectClient(this);
+                tcpclient.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public void SetUserName()
+        {
+
+            NetworkStream n = tcpclient.GetStream();
+            BinaryWriter w = new BinaryWriter(n);
+            w.Write("Hello new user. Please set a username: ");
+            string username = new BinaryReader(n).ReadString();
+            Name = username;
+            w.Flush();
+
+        }
+
+    }
+}
--- Networking_server/Program.cs
+++ Networking_server/Program.cs
@@ -7,6 +7,7 @@ using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Hangman;
 
 namespace Networking_server
 {
@@ -102,79 +103,6 @@ namespace Networking_server
                 Console.WriteLine($"{client.Name} has left the building...");
                 Broadcast(client, $"{client.Name} has left the building...");
             }
-        }
-
-        public class ClientHandler
-        {
-            public TcpClient tcpclient;
-            private Server myServer;
-            public string Name { get; set; }
-
-            public ClientHandler(TcpClient c, Server server)
-            {
-                tcpclient = c;
-                this.myServer = server;
-            }
-
-            public void Run()
-            {
-                try
-                {
-                    string message = "";
-                    bool firstLogin = true;
-
-                    while (firstLogin)
-                    {
-                        SetUserName();
-                        firstLogin = false;
-                    }
-
-
-                    while (!message.Equals("quit"))
-                    {
-
-                        NetworkStream n = tcpclient.GetStream();
-                        message = new BinaryReader(n).ReadString();
-                        if (message.Split(' ')[0].ToLower().Equals("pm"))
-                        {
-                            var msg = "";
-                            for (int i = 0; i < message.Split(' ').Length; i++)
-                            {
-                                if (i > 1)
-                                {
-                                    msg += message.Split(' ')[i] + " ";
-                                }
-                            }
-                            myServer.BroadcastPrivate(this, msg, message.Split(' ')[1]);
-                        }
-                        else
-                        {
-                            myServer.Broadcast(this, message);
-                        }
-                        Console.WriteLine(message);
-                    }
-
-                    myServer.DisconnectClient(this);
-                    tcpclient.Close();
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
-            }
-
-            public void SetUserName()
-            {
-
-                NetworkStream n = tcpclient.GetStream();
-                BinaryWriter w = new BinaryWriter(n);
-                w.Write("Hello new user. Please set a username: ");
-                string username = new BinaryReader(n).ReadString();
-                Name = username;
-                w.Flush();
-
-            }
-
         }
     }
 }
